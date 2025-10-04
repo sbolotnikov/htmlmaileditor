ca64fff0aed2463ba4c7e763b9d75fbaba0bfee5
Sergey Bolotnikov <sbolotnikov@gmail.com>
2025-10-04 02:22:29 -0400

    initial nextjs setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="absolute inset-0 flex flex-col justify-center items-center min-h-screen min-w-full ">
+      <h1 className="text-4xl font-bold mb-4">Welcome  to My Next.js App!</h1>
+    </div>
+  );
+}
